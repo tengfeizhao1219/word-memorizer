8f8e302683903bfe3c21c5d72c5d67da0081a559
tengfeizhao1219 <tengfeizhao1219@github.com>
2026-03-22 18:13:46 +0800

    🚀 添加完全干净的部署包 - 无任何密钥

---
+++ deploy-clean/cloud-functions/init-database/index.js
@@ -0,0 +1,11 @@
+// init-database 云函数 - 干净版本
+const cloud = require('wx-server-sdk');
+cloud.init({ env: cloud.DYNAMIC_CURRENT_ENV });
+
+exports.main = async (event, context) => {
+  return {
+    success: true,
+    message: 'init-database 函数已部署',
+    note: '这是干净版本，不包含任何密钥'
+  };
+};
---
+++ deploy-clean/cloud-functions/user-login/index.js
@@ -0,0 +1,11 @@
+// user-login 云函数 - 干净版本
+const cloud = require('wx-server-sdk');
+cloud.init({ env: cloud.DYNAMIC_CURRENT_ENV });
+
+exports.main = async (event, context) => {
+  return {
+    success: true,
+    message: 'user-login 函数已部署',
+    note: '这是干净版本，不包含任何密钥'
+  };
+};
